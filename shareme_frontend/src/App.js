784d73c602f2b0b0baea7b3511d8bdf6cfc156d0
Lakshay <lakshay@debian-BULLSEYE-live-builder-AMD64>
2022-08-24 22:58:49 +0530

    add index.js app.js index.css

---
+++ shareme_frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <h1 className="text-3x1 font-bold underline">
+        hello world.
+    </h1>
+  )
+}
+
+export default App
